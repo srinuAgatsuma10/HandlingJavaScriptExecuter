56071743bd9e5d0198b5d6c0a9c5734737287a0c
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-20 20:51:08 +0530

    OnePiece Wikipedia SetUp

---
+++ src/test/java/OnePiece_Wikipedia/HandlingJSExecuter.java
@@ -0,0 +1,47 @@
+package OnePiece_Wikipedia;
+
+import java.time.Duration;
+import java.util.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HandlingJSExecuter {
+
+	WebDriver driver;
+	
+	JavascriptExecutor jse;
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://en.wikipedia.org/wiki/One_Piece");
+		driver.manage().window().maximize();
+		// Initiate JavaScrip Executer
+		jse = (JavascriptExecutor) driver;
+	}
+
+	@Test(priority = 1)
+	public void handlingInputBox() {
+		
+	}
+
+	@Test(priority = 2)
+	public void radioButton() {
+		
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
